32a11821c6dcb95613539f2aa5fe2aed07d7ff2a
Quy Le <xuanquytt2410@gmail.com>
2022-11-07 16:00:49 +0700

    delete all server

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const productRoute= require('./product.route');
-
-function routes(app){
-    
-    app.use('/product', productRoute);
-    // app.use('/product', (req, res, next) => {res.send("hello world")});
-    
-}
-module.exports = routes;
